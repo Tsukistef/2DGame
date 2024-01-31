3237d5811b0a85b59e2fb45bd2d0c7a4ce165007
Stefania <s.fiorucci.art@gmail.com>
2024-01-31 22:31:57 +0000

    New sprites player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,7 +1,10 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -16,31 +19,74 @@ public class Player extends Entity {
 	this.gp = gp;
 	this.keyH = keyH;
 	setDefaultValues();
+	getPlayerImage();
 	}
 	
 	public void setDefaultValues() {
 		x = 100;
 		y = 100;
 		speed = 4;
+		direction = "down";
+	}
+	
+	public void getPlayerImage() { //method to associate each image with direction movement
+		try {
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down2.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left2.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right2.png"));
+			
+			System.out.println(up1);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void update() {
 		if(keyH.upPressed == true) {
+			direction = "up";
 			y -= speed;
 		}
 		if(keyH.downPressed == true) {
+			direction = "down";
 			y += speed;
 		}
 		if(keyH.leftPressed == true) {
+			direction = "left";
 			x -= speed;
 		}
 		if(keyH.rightPressed == true) {
+			direction = "right";
 			x += speed;
 		}
 	}
 		
 		public void draw(Graphics2D g2) {
-			g2.setColor(Color.white);
-			g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+			//g2.setColor(Color.white);
+			//g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+			
+			BufferedImage image = null;
+			
+			switch(direction) {
+			case "up":
+				image = up1;
+				break;
+			case "down":
+				image = down1;
+				break;
+			case "left":
+				image = left1;
+				break;
+			case "right":
+				image = right1;
+				break;
+			}
+			
+			g2.drawImage(image,  x,  y,  gp.tileSize, gp.tileSize, null);
 		}
 }
