22ad973800307ade07ee18244b6a626713b3d850
Stef <s.fiorucci.art@gmail.com>
2024-03-07 21:20:30 +0000

    debug

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -6,6 +6,8 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
 
 	public boolean upPressed, downPressed, leftPressed, rightPressed;
+	boolean checkDrawTime;
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -26,6 +28,16 @@ public class KeyHandler implements KeyListener {
 		 if(code == KeyEvent.VK_D) {
 			 rightPressed = true;
 		 }
+		 
+		 //DEBUG
+		 if(code == KeyEvent.VK_T) {
+			 if(checkDrawTime == false) {
+				 checkDrawTime = true;
+			 }
+			 else if(checkDrawTime == true) {
+				 checkDrawTime = false;
+			 }
+		 }	 
 	}
 
 	@Override
